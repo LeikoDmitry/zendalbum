e2d536c904d235da603c08e9c6349ce6bcd7abcc
Dmitry <leik.83@mail.ru>
2015-11-12 18:49:56 +0300

    create project is zendframework skeleton application by zendAlbum

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Файл конфигурации
+ */
+return array(
+    // Контроллеры
+    'controllers' => array(
+        'invokables' => array(
+            'Album\Controller\Album' => 'Album\Controller\AlbumController',
+        ),
+    ),
+    // Маршруты
+    'router' => array(
+        'routes' => array(
+            'album' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/album[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Album\Controller\Album',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    // Виды
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
+
+);
